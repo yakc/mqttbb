7092e911fe1671e30eea8b916d65696f472ddae6
yakc <yakc@users.noreply.github.com>
2024-04-18 01:05:53 -0700

    add JsonSubscription and executeAll
    as an easy way to subscribe to any number of topics that expect
    JSON payloads, asynchronously.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const bb = require("./lib/barebones");
-const { Echo, sendOne } = require("./lib/pubsub");
+const { executeAll, JsonSubscription, QoS, Echo, sendOne } = require("./lib/pubsub");
 
 const args = {
   topic: "sdk/test/js",
@@ -10,11 +10,26 @@ const args = {
 async function main(argv) {
   const connection = await bb.connect();
 
-  // Bare minimum single message, or more complicated multi-message object
-  console.log(await sendOne(connection, argv.topic, argv.message));
-  // await new Echo(argv.topic, argv.message, argv.count).execute(connection);
+  // Bare minimum single message
+  //   console.log(await sendOne(connection, argv.topic, argv.message));
+  // More complicated multi-message object
+  //   await new Echo(argv.topic, argv.message, argv.count).execute(connection);
+  // Any number of subscription callbacks, started async
+  const subs = executeAll(connection, [
+    new JsonSubscription(args.topic,  // any number, but just one here
+      (json, topic, dup, qos, retain) => {
+        console.log(topic, dup, qos, retain, json);
+      }),
+  ]).catch(x => {
+    console.error(x);
+  });
 
-  await bb.disconnect(connection);
+  // Simple publish to a single topic; await to preserve order
+  await connection.publish(args.topic, args, QoS.AtLeastOnce);  // JSON payload
+  await connection.publish(args.topic, args.message, QoS.AtLeastOnce);  // not JSON
+
+  await subs;  // wait for stuff to happen until reject, then...
+  await bb.disconnect(connection);  // ...tear everything down
 }
 
 main(args);
